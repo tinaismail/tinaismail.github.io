08a40d211640c8fbcda047e1ae62cf194be8a996
tinaismail <ismait1@mcmaster.ca>
2022-08-19 17:12:11 +0300

    Replace all occurrances of old name to new name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'personal-website'`, () => {
+  it(`should have as title 'tinaismail.github.io'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('personal-website');
+    expect(app.title).toEqual('tinaismail.github.io');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('personal-website app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('tinaismail.github.io app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'personal-website';
+  title = 'tinaismail.github.io';
 }
